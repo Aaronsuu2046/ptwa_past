50a16c8564d1f3013059434a4b1369ee7217947e
aaron <aaronsuu2046@gmail.com>
2022-10-28 00:12:06 +0800

    1028
    add main page & reload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById("game1").onclick = function(){
     document.getElementById('frame1').style.display = "block";
     document.getElementById('frame2').style.display = "none";
+    document.getElementById('frame3').style.display = "none";
 }
 
 document.getElementById("game2").onclick = function(){
     document.getElementById('frame2').style.display = "block";
     document.getElementById('frame1').style.display = "none";
+    document.getElementById('frame3').style.display = "none";
 }
