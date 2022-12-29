7fc130ff93a36c873f9994aac007ca54048346e3
Jesse <jessie97013@gmail.com>
2022-12-29 11:20:45 +0800

    Add Score Setting

--- digital_connection/Game.js
+++ digital_connection/Game.js
@@ -13,10 +13,11 @@ class Game{
         this.numbers = [];
         this.topic_points = [];
         this.ans_points = [];
+        this.topic_point = 0;
+        this.ans_point = 0;
         this.startPosition = {x: 0, y: 0};
         this.lineCoordinates = {x: 0, y: 0};
         this.isDrawStart = false;
-        this.isDrawEnd = false;
         // create objects
         this.npc = new Npc();
         let y = 80;
@@ -31,18 +32,31 @@ class Game{
     ready(){
         // view of ready to start
         ctx_bg.fillStyle = BLUE;
-        ctx_bg.font = FONT_STYLE;
-        ctx_bg.fillText('準備好了嗎?', canvas.width/3, canvas.height / 2);
+        ctx_bg.font = H1_FONT_STYLE;
+        ctx_bg.fillText('準備好了嗎?', canvas_bg.width/3, canvas_bg.height / 2);
         startBtn.addEventListener('click', (e) => {
-            ctx_bg.clearRect(0, 0, canvas.width, canvas.height);
+            ctx_bg.clearRect(0, 0, canvas_bg.width, canvas_bg.height);
             ctx_bg.fillText('將數字連上正確的手勢吧！', this.npc.x + this.npc.width+this.npc.width/2, this.npc.height);
+            ctx_bg.fillStyle = RED;
+            ctx_bg.font = SCORE_FONT_STYLE;
+            ctx_bg.fillText(this.score, canvas_bg.width - 80, 50);
             this.draw_bg();
         })
     }
     
     animate(){
         this.draw_view();
+        this.get_result();
         requestAnimationFrame(()=>this.animate());
+    }
+
+    get_result(){
+        if (this.score !== 100) {
+            return
+        }
+        ctx_current.fillStyle = RED;
+        ctx_current.font = H1_FONT_STYLE;
+        ctx_current.fillText("恭喜過關！", canvas_current.width/3, canvas_current.height / 2);
     }
 
     draw_bg(){
@@ -57,8 +71,8 @@ class Game{
     draw_view(){  
         this.getClientOffset = (e) => {
             let {pageX, pageY} = e.touches ? e.touches[0] : e;
-            let x = pageX - canvas_3.offsetLeft;
-            let y = pageY - canvas_3.offsetTop;
+            let x = pageX - canvas_current.offsetLeft;
+            let y = pageY - canvas_current.offsetTop;
         
             return {
                x,
@@ -66,22 +80,22 @@ class Game{
             } 
         }  
         this.drawLine = () => {
-            ctx_line.strokeStyle = BLACK;
-            ctx_line.lineWidth = 3;
-            ctx_line.beginPath();
-            ctx_line.moveTo(this.startPosition.x, this.startPosition.y);
-            ctx_line.lineTo(this.lineCoordinates.x, this.lineCoordinates.y);
-            ctx_line.stroke();
+            ctx_current.strokeStyle = BLACK;
+            ctx_current.lineWidth = 3;
+            ctx_current.beginPath();
+            ctx_current.moveTo(this.startPosition.x, this.startPosition.y);
+            ctx_current.lineTo(this.lineCoordinates.x, this.lineCoordinates.y);
+            ctx_current.stroke();
         }
         
         this.mouseDownListener = (e) => {
             this.startPosition = this.getClientOffset(e);
-            if (this.collide_with_topic_point(this.startPosition)){
+            this.topic_point = this.collide_with_topic_point(this.startPosition);
+            if (this.topic_point){
                 this.isDrawStart = true;
-                this.lineCoordinates = null;
             }
             else{
-                this.clearCanvas();
+                this.clear_current_canvas();
             }
         }
         
@@ -89,56 +103,78 @@ class Game{
             if(!this.isDrawStart) return;
             
             this.lineCoordinates = this.getClientOffset(e);
-            this.clearCanvas();
+            this.clear_current_canvas();
             this.drawLine();
         }
         
         this.mouseupListener = (e) => {
             this.isDrawStart = false;
-            this.isDrawEnd = true;
-            // TODO: refactor be only for one time
-            if (!this.collide_with_topic_point(this.startPosition) || !this.collide_with_ans_point(this.lineCoordinates)) {
+            this.ans_point = this.collide_with_ans_point(this.lineCoordinates);
+            if (!this.topic_point || !this.ans_point) {
                 return;
             }
-
-            if (this.collide_with_topic_point(this.startPosition) === this.collide_with_ans_point(this.lineCoordinates)){
-                // TODO: Test before clear or save
+            if (this.topic_point === this.ans_point){
                 this.draw_correct_line();
-                this.isDrawEnd = false;
-                this.clearCanvas();
+                this.add_score(20);
+                document.getElementById('correct').play();
             }
-            else if (this.collide_with_topic_point(this.startPosition) !== this.collide_with_ans_point(this.lineCoordinates)) {
+            else {
                 this.draw_wrong_line();
+                document.getElementById('wrong').play();
             }
+            this.topic_point = 0;
+            this.ans_point = 0;
         }
         
-        this.clearCanvas = () => {
-           ctx_line.clearRect(0, 0, canvas_3.width, canvas_3.height);
+        this.clear_current_canvas = () => {
+           ctx_current.clearRect(0, 0, canvas_current.width, canvas_current.height);
         }
            
         this.draw_correct_line = () => {
-            if (!this.isDrawEnd) return;
-            ctx_correct_line.strokeStyle = GREEN;
-            ctx_correct_line.lineWidth = 5;
-            ctx_correct_line.beginPath();
-            ctx_correct_line.moveTo(this.startPosition.x, this.startPosition.y);
-            ctx_correct_line.lineTo(this.lineCoordinates.x, this.lineCoordinates.y);
-            ctx_correct_line.stroke();
+            ctx_stay.strokeStyle = GREEN;
+            ctx_stay.lineWidth = 5;
+            ctx_stay.beginPath();
+            ctx_stay.moveTo(this.startPosition.x, this.startPosition.y);
+            ctx_stay.lineTo(this.lineCoordinates.x, this.lineCoordinates.y);
+            ctx_stay.stroke();
         }
         this.draw_wrong_line = () => {
-            if (!this.isDrawEnd) return;
-            ctx_line.strokeStyle = 'red';
-            ctx_line.lineWidth = 5;
-            ctx_line.beginPath();
-            ctx_line.moveTo(this.startPosition.x, this.startPosition.y);
-            ctx_line.lineTo(this.lineCoordinates.x, this.lineCoordinates.y);
-            ctx_line.stroke();
+            ctx_current.strokeStyle = RED;
+            ctx_current.lineWidth = 5;
+            ctx_current.beginPath();
+            ctx_current.moveTo(this.startPosition.x, this.startPosition.y);
+            ctx_current.lineTo(this.lineCoordinates.x, this.lineCoordinates.y);
+            ctx_current.stroke();
         }
-        canvas_3.addEventListener('mousedown', this.mouseDownListener);
-        canvas_3.addEventListener('mousemove', this.mouseMoveListener);
-        canvas_3.addEventListener('mouseup', this.mouseupListener);
+        canvas_current.addEventListener('mousedown', this.mouseDownListener);
+        canvas_current.addEventListener('mousemove', this.mouseMoveListener);
+        canvas_current.addEventListener('mouseup', this.mouseupListener);
     }
     
+    add_score(score){
+        this.score += score;
+        ctx_bg.clearRect(canvas_bg.width - 100, 0, 100, 100);
+        ctx_bg.fillStyle = GREEN;
+        ctx_bg.font = SCORE_FONT_STYLE;
+        ctx_bg.fillText(this.score, canvas_bg.width - 80, 50);
+        this.clear_current_canvas();
+    }
+
+    check_next_level(){
+        document.querySelectorAll('button').forEach(e => {
+            let id = e.getAttribute('id');
+            e.addEventListener('click', function() {
+                if (id === 'Btn3'){
+                    document.getElementById('correct').play();
+                    alert('太棒了!!');
+                    }
+                    if (id === "Btn1" || id === "Btn2"|| id === "Btn4"|| id === "Btn5"
+                    || id === "Btn6"|| id === "Btn7"|| id === "Btn8"|| id === "Btn9"){
+                        document.getElementById('wrong').play();
+                    }
+            });
+        });
+    }
 
     collide_with_topic_point(startPosition){
         for (let i = 0; i < this.topic_points.length; i++) {
@@ -147,7 +183,7 @@ class Game{
                 return point.ans;
             }
         }
-        return false;
+        return 0;
     }
 
     collide_with_ans_point(startPosition){
@@ -157,7 +193,7 @@ class Game{
                 return point.ans;
             }
         }
-        return false;
+        return 0;
     }
 
     collide_with_x(mos_pos, point){
@@ -176,7 +212,7 @@ class Game{
 
     create_ans(i, y){
         let path = IMAGE_PATH + "ans_" + i + ".jpg";
-        let gesture = new Gesture(canvas.width, y, path);
+        let gesture = new Gesture(canvas_bg.width, y, path);
         gesture.x -= gesture.width + 50
         this.gestures.push(gesture);
         this.create_point(i, gesture.x-50, gesture.y+25, this.ans_points);
@@ -202,23 +238,6 @@ function level1 (){
     });
 }
 level1();
-
-function checkAnswer(){
-    document.querySelectorAll('button').forEach(e => {
-        let id = e.getAttribute('id');
-        e.addEventListener('click', function() {
-        if (id === 'Btn3'){
-            document.getElementById('correct').play();
-            alert('太棒了!!');
-            }
-            if (id === "Btn1" || id === "Btn2"|| id === "Btn4"|| id === "Btn5"
-            || id === "Btn6"|| id === "Btn7"|| id === "Btn8"|| id === "Btn9"){
-                document.getElementById('wrong').play();
-            }
-        });
-    });
-}
-
 
 function level2 (){
     const nextLevelBtn = document.getElementById('nextLevel');
