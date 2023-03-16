e3f85acee5c5e5a4bdb4af864c389e89e1f8a24e
Jesse <jessie97013@gmail.com>
2023-03-16 22:59:03 +0800

    Add touch event

--- recognize_milliliter/script.js
+++ recognize_milliliter/script.js
@@ -10,7 +10,7 @@ const water_scale = document.querySelector(`.water_scale`);
 const water_control = document.querySelector('.water_control');
 const topic = document.querySelector('.topic');
 const scales = document.querySelector('.scales');
-let level = 1, milliliter = 5, start = 0, end = 10, tolerance = 1;
+let level = 1, milliliter = 5, start = 0, end = 10, tolerance = 1, delay = 40;
 let act = '';
 let answer = getRandomNumber();
 
@@ -57,6 +57,42 @@ water_control.addEventListener('wheel', (e) => {
     water_scale.textContent = `${milliliter}`;
 });
 
+let active = false;
+let lastTouchY, currentTouchY = null;
+
+water_control.addEventListener('touchstart', (e) => {
+    lastTouchY = e.touches[0].clientY;
+    active = true;
+});
+
+water_control.addEventListener('touchmove', (e) => {
+    if (active) {
+        if (milliliter>end){
+            return
+        }
+        currentTouchY = e.touches[0].clientY;
+        if (lastTouchY !== null) {
+            const deltaY = currentTouchY - lastTouchY;
+            if (deltaY < -delay && milliliter<end) {
+                // moved up
+                milliliter += tolerance;
+                lastTouchY = currentTouchY;
+        } else if (deltaY > delay && milliliter>start) {
+                // moved down
+                milliliter -= tolerance;
+                lastTouchY = currentTouchY;
+            }
+        }
+
+        water_scale.style.transform = `translate(0%, ${100-((start+milliliter)/end)*100}%)`;
+        water.style.height = `${((start+milliliter)/end)*100}%`;
+        water_scale.textContent = `${milliliter}`;
+        }
+});
+
+water_control.addEventListener('touchend', () => {
+    active = false;
+});
 
 function backLevel() {
     if (level<=1) {
@@ -99,27 +135,27 @@ function checkAnswer() {
 
 function changeLevel() {
     if (level === 1){
-        start = 0;
+        delay = 40;
         end = 10;
         tolerance = 1;
     }
     else if (level === 2){
-        start = 0;
+        delay = 40;
         end = 50;
         tolerance = 5;
     }
     else if (level === 3){
-        start = 0;
+        delay = 10;
         end = 300;
         tolerance = 10;
     }
     else if (level === 4){
-        start = 0;
+        delay = 20;
         end = 1000;
         tolerance = 50;
     }
     else if (level === 5){
-        start = 0;
+        delay = 20;
         end = 2000;
         tolerance = 100;
     }
