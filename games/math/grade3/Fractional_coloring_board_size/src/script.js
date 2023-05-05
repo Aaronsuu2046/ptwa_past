2e268df65b24cd68ada69de2a49ae2411639163a
Aka2210 <qoo0973215128@gmail.com>
2023-05-05 21:48:32 +0800

    Click  judge of graphics

--- games/math/grade3/Fractional_coloring_board_size/src/script.js
+++ games/math/grade3/Fractional_coloring_board_size/src/script.js
@@ -27,28 +27,51 @@ jumpBtn.on('animationiteration', ()=>{
     setTimeout(()=>{jumpBtn.css('animation-play-state', 'running');}, 2000);
 });
 
-$('path').on('click', (index) => {
-    console.log(index);
+$('.circleFraction svg').on('click', (e) => {
+    let nowPearClick = $(e.target).index();
+    console.log(nowPearClick);
+    if($(e.target).attr('class') === 'uncolored'){
+        $(e.target).css('fill', '#dc0073');
+        $(e.target).attr('class', 'colored');
+    }
+    else{
+        $(e.target).css('fill', '#8e8e9c');
+        $(e.target).attr('class', 'uncolored');
+    }    
 })
-// comparisonSymbol.on('mousedown', (e) =>{
-//     let initX = e.clientX, initY = e.clientY;
-//     if(e.target.className !== 'answerDisplay'){
-//         $('body').on('mousemove', (event) => {
-//             //左下角:(349, 317) 右上角:(449, 215)
-//             $('.' + e.target.className).css('transform', 'translate(' 
-//             + (event.clientX - initX) + 'px,' + (event.clientY - initY) +'px)');
-//         })
 
-//         $('body').on('mouseup', (temp) => {
-//             let endPositionX = temp.clientX, endPositionY = temp.clientY;
-//             // console.log(endPositionX, endPositionY); //用來鎖定答案框範圍
-//             $('.' + e.target.className).off();
-//             $('body').off();
-//             $('.' + e.target.className).css('transform', 'translate(0px, 0px)');
-//             if((parseInt(endPositionX) >= 345 && parseInt(endPositionX) <= 450) 
-//             && (parseInt(endPositionY) <= 320 && parseInt(endPositionY) >= 215)){
-//                 game.symbolDisplay(temp.target.className);
-//             }
-//         })
-//     }
-// })
+$('.pear img').on('click', (e) => {
+    let nowPearClick = $(e.target).index();
+    console.log(nowPearClick);
+    if($(e.target).attr('class') === 'uncolored'){
+        $(e.target).attr('src', 'src/fractionImg/pearColor.png');
+        $(e.target).attr('class', 'colored');
+    }
+    else{
+        $(e.target).attr('src', 'src/fractionImg/pear.png');
+        $(e.target).attr('class', 'uncolored');
+    }
+})
+
+comparisonSymbol.on('mousedown', (e) =>{
+    let initX = e.clientX, initY = e.clientY;
+    if(e.target.className !== 'answerDisplay'){
+        $('body').on('mousemove', (event) => {
+            //左下角:(349, 492) 右上角:(449, 394)
+            $('.' + e.target.className).css('transform', 'translate(' 
+            + (event.clientX - initX) + 'px,' + (event.clientY - initY) +'px)');
+        })
+
+        $('body').on('mouseup', (temp) => {
+            let endPositionX = temp.clientX, endPositionY = temp.clientY;
+            // console.log(endPositionX, endPositionY); //用來鎖定答案框範圍
+            $('.' + e.target.className).off();
+            $('body').off();
+            $('.' + e.target.className).css('transform', 'translate(0px, 0px)');
+            if((parseInt(endPositionX) >= 345 && parseInt(endPositionX) <= 450) 
+            && (parseInt(endPositionY) <= 495 && parseInt(endPositionY) >= 390)){
+                game.symbolDisplay(temp.target.className);
+            }
+        })
+    }
+})
