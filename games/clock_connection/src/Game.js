d8bff2e05101dc7747d040f531aa214dcb6e7b48
Jesse <jessie97013@gmail.com>
2023-05-25 21:34:27 +0800

    Finish clock connection

--- games/clock_connection/src/Game.js
+++ games/clock_connection/src/Game.js
@@ -14,87 +14,55 @@ export class Game extends ConnectionGame {
         this.topicExplain = Array(this.levelLimit).fill('時鐘上的刻度是幾點幾分呢？');
     
         this.leftArea = $('.gameArea .leftArea');
-        this.centerArea = $('.gameArea .centerArea');
         this.rightArea = $('.gameArea .rightArea');
     }
 
     getCorrectLimit(){
-        return this.gameData[this.level-1].thousand.length;
+        return this.gameData[this.level-1].hour.length;
     }
 
     startGame(level) {
         super.startGame(level);
-        this.resetQuestions();
         this.createQuestions();
         this.createHint();
         helpModules.reorder(
             this.leftArea
-            , this.centerArea
             , this.rightArea
         );
     }
 
     creatRecord(recordType, dot){
         const value = dot.data('value');
-        console.log(value);
-        console.log(this.recordObj.record);
         return `${value}`;
-    }
-
-    resetQuestions() {
-        this.leftArea.find('*').remove('img');
-        this.centerArea.find('*').filter(':hidden').css('display', '');
-        this.rightArea.find('*').filter(':hidden').css('display', '');
     }
 
     createQuestions() {
         const level = this.level - 1;
-        const $gameArea = $('.gameArea');
-        for (let i = 0; i < 4; i++) {
-            const thousand = helpModules.randomNumber(0, this.gameData[level].thousand[i]);
-            const hundred = helpModules.randomNumber(0, this.gameData[level].hundred[i]);
-            const ten = helpModules.randomNumber(0, this.gameData[level].ten[i]);
-            const ones = helpModules.randomNumber(0, this.gameData[level].ones[i]);
-            // console.log(area);
-            this.createImg(this.leftArea.find('.contentArea').eq(i), "thousand", thousand);
-            this.createImg(this.leftArea.find('.contentArea').eq(i), "hundred", hundred);
-            this.createImg(this.leftArea.find('.contentArea').eq(i), "ten", ten);
-            this.createImg(this.leftArea.find('.contentArea').eq(i), "ones", ones);
-            thousand === 0 ? this.centerArea.find('.thousand').eq(i).css('display', 'none') :this.centerArea.find('.thousand .number').eq(i).text(thousand);
-            hundred === 0 ? this.centerArea.find('.hundred').eq(i).css('display', 'none') :this.centerArea.find('.hundred .number').eq(i).text(hundred);
-            ten === 0 ? this.centerArea.find('.ten').eq(i).css('display', 'none') :this.centerArea.find('.ten .number').eq(i).text(ten);
-            ones === 0 ? this.centerArea.find('.ones').eq(i).css('display', 'none') :this.centerArea.find('.ones .number').eq(i).text(ones);
-            thousand === 0 ? this.rightArea.find('.thousand').eq(i).css('display', 'none') :this.rightArea.find('.thousand .number').eq(i).text(thousand);
-            hundred === 0 ? this.rightArea.find('.hundred').eq(i).css('display', 'none') :this.rightArea.find('.hundred .number').eq(i).text(hundred);
-            ten === 0 ? this.rightArea.find('.ten').eq(i).css('display', 'none') :this.rightArea.find('.ten .number').eq(i).text(ten);
-            ones === 0 ? this.rightArea.find('.ones').eq(i).css('display', 'none') :this.rightArea.find('.ones .number').eq(i).text(ones);
-            if (i<3){
-                this.leftArea.find('.questionArea').eq(i).find('.dot').data({'value': `${thousand}${hundred}${ten}${ones}`});
-                this.centerArea.find('.questionArea').eq(i).find('.dot').data({'value': `${thousand}${hundred}${ten}${ones}`});
-                this.rightArea.find('.questionArea').eq(i).find('.dot').data({'value': `${thousand}${hundred}${ten}${ones}`});
-            }
+        for (let i = 0; i < this.correctLimit; i++) {
+            let hour = helpModules.randomNumber(1, this.gameData[level].hour[i]);
+            let minute = helpModules.randomNumber(1, this.gameData[level].minute[i]);
+            this.setTime(hour, minute, this.leftArea.find('.contentArea').eq(i));
+            hour = hour < 10 ? `0${hour}` : hour;
+            minute = minute < 10 ? `0${minute}` : minute;
+            this.rightArea.find('.contentArea').eq(i).find('.hour').text(hour);
+            this.rightArea.find('.contentArea').eq(i).find('.minute').text(minute);
+            this.leftArea.find('.questionArea').eq(i).find('.dot').data({'value': `${hour}:${minute}`});
+            this.rightArea.find('.answerArea').eq(i).find('.dot').data({'value': `${hour}:${minute}`});
         };
     }
 
-    createImg(unit, imgName, count) {
-        if (count < 0){
-            unit.css('display', 'none');
-            return
-        }
-        for (let i = 0; i < count; i++) {
-            unit.append(this.getImg(imgName));
-        }
-    }
-
-    getImg(imgName) {
-        return $('<img>')
-            .attr('src', `./assets/${imgName}.png`)
-            .attr('alt', imgName);
-    }
+    setTime(hour, minute, targetElement) {
+        // 計算指針的角度
+        const hoursDegree = (hour / 12) * 360;
+        const minutesDegree = (minute / 60) * 360;
+      
+        // 旋轉指針
+        targetElement.find('.hour').css('transform', `rotate(${hoursDegree}deg)`);
+        targetElement.find('.minute').css('transform', `rotate(${minutesDegree}deg)`);
+      }
       
     createHint() {
         $('.hintContainer .leftArea').html(this.gameArea.find('.leftArea .contentArea').clone());
-        $('.hintContainer .centerArea').html(this.gameArea.find('.centerArea .contentArea').clone());
         $('.hintContainer .rightArea').html(this.gameArea.find('.rightArea .contentArea').clone());
     }
 }
