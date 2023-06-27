f2b0d4ad19c8d6e8006c33d3773d6e631580c5fb
Jesse <jessie97013@gmail.com>
2023-06-27 16:01:07 +0800

    Prototype 17

---
+++ games/17/src/Game.js
@@ -0,0 +1,59 @@
+// Import all you need module in game
+import * as constant from "../../../game_view/src/constant.js"
+import {
+    gameModules
+    , helpModules
+} from "../../../game_view/src/module.js"
+import {
+    CombinationLockTemplate
+} from "../../../game_view/src/templates/CombinationLockTemplate.js"
+
+
+// Export your game
+export class Game extends CombinationLockTemplate {
+    constructor(gameData){
+        super(gameData);
+        // Initialise game object
+        this.topicExplain = Array(this.levelLimit).fill("拖動圖形比對方格圖，答案是幾平方公分呢？");
+        $( ".topic img" ).draggable();
+    }
+
+    startGame(level) {
+        super.startGame(level);
+        // create game content
+        this.answerData = this.gameData[level-1].answer;
+        this.generatorTopicArea();
+    }
+
+    generatorTopicArea() {
+        //future
+        this.topArea.children().hide();
+        this.topArea.children().eq(this.level - 1).show();
+    }
+
+    correctAnswer(){
+        // action
+        this.recordObj.appendToRecord(constant.recordItim.QUESTION, this.answerData);
+        this.recordObj.appendToRecord(constant.recordItim.ANSWER, this.currentAnswer);
+    }
+    wrongAnswer(){
+        // action
+        this.recordObj.appendToRecord(constant.recordItim.QUESTION, this.answerData);
+        this.recordObj.appendToRecord(constant.recordItim.ANSWER, this.currentAnswer);
+    }
+
+}
+
+export default Game;
+
+// Read game_config.json file
+const gameData = await gameModules.getJson('../../games/17/game_config.json');
+// Create game Instance, Must input gameData
+const gameInstance = new Game(gameData);
+// Post game to game_view
+window.getGame = function() {
+    return gameInstance;
+};
+
+// type must === game filename
+parent.postMessage({ type: '17' }, '*');
