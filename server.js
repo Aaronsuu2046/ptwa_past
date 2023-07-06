f74975989d83693b91280ee67a1db3ee1c22e97b
ivanchiou <wyattkidd@gmail.com>
2023-07-06 22:40:55 +0800

    fixed the static file path of route on express

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+});
 var port = 8080; // you can use any port
 app.listen(port);
 console.log('server on ' + port);
